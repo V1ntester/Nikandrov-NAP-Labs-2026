09c8c89440fc958ca3c91507196b526a0316be08
V1ntester <valentin_n_valen@mail.ru>
2026-04-26 15:04:13 +0300

    (+) Inited three.js. Added model to product page

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,11 +1,14 @@
 import { BackButtonComponent } from "../../components/back-button/index.js";
 import { MainPage } from "../main/index.js";
 import { ProductComponent } from "../../components/product/index.js";
+import { ModelComponent } from "../../components/model/index.js";
 
 export class ProductPage {
   constructor(parent, data) {
     this.parent = parent;
     this.data = data;
+
+    this.modelInstance = null;
   }
 
   getData() {
@@ -20,11 +23,17 @@ export class ProductPage {
     return document.getElementById("product-card-container");
   }
 
+  get modelContainer() {
+    return document.getElementById("model-container");
+  }
+
   getHTML() {
     return `
                 <div id="product-page" class="mt-4">
                     <div class="container">
                         <div id="product-card-container">
+                        </div>
+                        <div id="model-container">
                         </div>
                     </div>
                 </div>
@@ -44,5 +53,12 @@ export class ProductPage {
     const data = this.getData();
     const stock = new ProductComponent(this.productCardContainer);
     stock.render(data, this.clickBack.bind(this));
+
+    if (this.modelInstance) {
+      this.modelInstance.destroy?.();
+    }
+
+    this.modelInstance = new ModelComponent(this.modelContainer);
+    this.modelInstance.render("/assets/models/red-car.glb");
   }
 }
