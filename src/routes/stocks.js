f49e47b6b7566e44b5e6be7c8f1040940ab1e269
V1ntester <valentin_n_valen@mail.ru>
2026-04-26 21:59:06 +0300

    (+) Added swagger

--- src/routes/stocks.js
+++ src/routes/stocks.js
@@ -2,11 +2,77 @@ const express = require('express');
 const router = express.Router();
 const stocksController = require('../controllers/stocksController');
 
-// Определение маршрутов
+/**
+ * @swagger
+ * /stocks:
+ *   get:
+ *     summary: Получить все товары
+ *     responses:
+ *       200:
+ *         description: Успешный ответ
+ */
 router.get('/', stocksController.getAllStocks);
+
+/**
+ * @swagger
+ * /stocks/{id}:
+ *   get:
+ *     summary: Получить товар по его Id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Успешный ответ
+ */
 router.get('/:id', stocksController.getStockById);
+
+/**
+ * @swagger
+ * /stocks:
+ *   post:
+ *     summary: Добавить товар
+ *     responses:
+ *       200:
+ *         description: Успешный ответ
+ */
 router.post('/', stocksController.createStock);
+
+/**
+ * @swagger
+ * /stocks/{id}:
+ *   patch:
+ *     summary: Частично обновить товар
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Успешный ответ
+ */
 router.patch('/:id', stocksController.updateStock);
+
+/**
+ * @swagger
+ * /stocks/{id}:
+ *   delete:
+ *     summary: Удалить товар
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Успешный ответ
+ */
 router.delete('/:id', stocksController.deleteStock);
 
 module.exports = router;
